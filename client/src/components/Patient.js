5e08d04c890ee04bec5ecdbe72ce22d1e01dfdb8
Edoumou <devforsam@gmail.com>
2021-05-16 17:16:04 +0200

    changes in the patient page

--- client/src/components/Patient.js
+++ client/src/components/Patient.js
@@ -1,8 +1,64 @@
 import React, { Component } from 'react';
-import { Card, Image, Segment } from 'semantic-ui-react';
-import headerImage from '../images/header2.jpeg'
+import { Header, Card, Image, Segment, Grid } from 'semantic-ui-react';
+import headerImage from '../images/header2.jpeg';
+import FetchFromIPFS from "./utils/FetchFromIPFS";
+import '../App.css';
+
+const iv = 16;
+const ENCRYPTION_KEY = 'fpbyr4386v8hpxdruppijkt3v6wayxmi';
 
 class Patient extends Component {
+    state = {
+        ID: '',
+        protocol: '',
+        fullName: '',
+        cid: '',
+        data: {}
+    }
+
+    getPatientData = async () => {
+        const nbOfPatients = await this.props.contract.methods.nbOfPatients()
+            .call({ from: this.props.account });
+
+        for (let i = 0; i < nbOfPatients; i++) {
+            const patientID = await this.props.contract.methods.patientsID(i)
+                .call({ from: this.props.account });
+
+            const patient = await this.props.contract.methods.patients(patientID)
+                .call({ from: this.props.account });
+
+            let cid;
+            if (patient.patient === this.props.account) {
+                let cids = [];
+                cids = await this.props.contract.methods.getDataCID(patientID)
+                    .call({ from: this.props.account });
+                cid = cids[0];
+
+                let index = cids.length - 1;
+                this.setState({ cid: cids[index] });
+
+                const encodedData = await FetchFromIPFS(this.state.cid, ENCRYPTION_KEY)
+
+                console.log("cid =", encodedData);
+
+                let data = JSON.parse(await FetchFromIPFS(cid, ENCRYPTION_KEY));
+
+                this.setState({
+                    ID: data.id,
+                    protocol: data.protocol,
+                    fullName: data.fullName
+                });
+
+                console.log("DATA =", this.state.ID, this.state.fullName, this.state.protocol);
+            }
+
+        }
+    }
+
+    componentDidMount = async () => {
+        await this.getPatientData();
+    }
+
     render() {
         return (
             <div className="promoter-admin">
@@ -18,6 +74,20 @@ class Patient extends Component {
                 <div className="header-img">
                     <Image src={headerImage} size="large" />
                 </div>
+
+                <div className="patient-grid">
+                    <Grid columns={2} divided textAlign="left">
+                        <Grid.Row stretched>
+                            <Grid.Column width={6}>
+                                <Header as='h2'>Patient Infos</Header>
+                            </Grid.Column>
+                            <Grid.Column width={10}>
+                                <Header as="h2">We are here</Header>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </div>
+
             </div>
         )
     }
